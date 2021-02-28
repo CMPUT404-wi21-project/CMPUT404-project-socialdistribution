3b1e856c5b09d130ba83fed3665551240bc823c1
PENGYUXIONG <pxiong@ualberta.ca>
2021-02-28 00:01:12 -0700

    add in more backend post, make redux store persistent - add formattedJSONpost, to format post json into correct json form - add in pagination feature for getting author's recent post, page size   set as 50 - add redux persistent to hold state in store

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,8 +7,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', index.index, name="index"),
-    path(r'author/<str:author_id>/posts/', simplePostView.createNewPost, name="post-post-view"),
-    path(r'author/<str:author_id>/posts/<str:post_id>', simplePostView.handleExistPost, name="get-post-view"),
+    path(r'author/<str:author_id>/posts/', simplePostView.handlePostByAuthorId, name="handle-posts-view"),
+    path(r'author/<str:author_id>/posts/<str:post_id>', simplePostView.handlePostByPostId, name="handle-single-post-view"),
     path(r'author/register/', simpleSignupRequestView.createSignupRequest, name="register-author"),
     path('api-auth/', include('rest_framework.urls')),    
     path('api/token/', TokenObtainPairView.as_view()),
